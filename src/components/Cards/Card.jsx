32542c0d1a4489b6b5b85c49fd1db620a7c63ee0
Gabriel Evangelista <86444407+GabrielEvangelista0@users.noreply.github.com>
2023-05-01 13:44:33 -0300

    update

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+export default function Card() {
+    
+    return (
+        <div className='card'>
+            <h2>data</h2>
+            <span>umidade: % </span>
+            <span>temperatura Max: °C</span>
+            <span>temperatura Min: °C</span>
+        </div>
+    )
+}
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,11 +2,11 @@ import './Form.css'
 
 export default function Form(props) {
     return (
-        <div role='form' className='formulario' >
+        <form className='formulario' onSubmit={props.onSubmit}>
             <input type="text" placeholder='Digite uma cidade' onChange={props.onChange} />
             <button onClick={props.onClick}>
                 buscar
             </button>
-        </div>
+        </form>
     )
 }
